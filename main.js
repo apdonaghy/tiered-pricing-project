1d8b4d6eec08c2336b0c71d3dd5bc35e56cd032b
apdonaghy <adonaghy@seattleu.edu>
2021-03-20 17:42:21 -0700

    consolidating code

--- main.js
+++ main.js
@@ -1,30 +1,90 @@
 let basePrice = document.getElementById('basePrice')
 let tierDiff = document.getElementById('tierDiff')
 const calculateBtn = document.getElementById('calculate')
+let tierPrices = [];
 
-let teirPrices = [];
 
+function runExchange(apidata){
 
-function calculateDiff(tierVal){
-       let price1 = document.getElementById('USD1')
-       price1.innerText = basePrice.value
-       let price2 = document.getElementById('USD2')
-       price2.innerText = (basePrice.value - tierVal).toFixed(2);
-       let price3 = document.getElementById('USD3')
-       price3.innerText = (basePrice.value - (tierVal * 2)).toFixed(2); 
-       let price4 = document.getElementById('USD4')
-       price4.innerText = (basePrice.value - (tierVal * 3)).toFixed(2); 
+    console.log(apidata['rates'])
+    const landed = document.getElementById('SGD')
+
+   
+    if(landed.value === ''){
+        // let noLanded = tierPrices[0]
+        // let apiRate = parseFloat(noLanded * apidata['rates']['SGD']).toFixed(2)
+        // console.log(apiRate)
+           let sgdDOM = document.querySelectorAll('.SGD1')
+           let item = 0;
+       
+   
+            for(let tier of tierPrices){
+                for(let singleSGD of sgdDOM){
+                    item = tier 
+
+                    singleSGD.innerText = item * parseFloat(apidata['rates']['SGD']).toFixed(2)
+                   
+               }
+              
+              
+            }
+         
+     
+           
+            
+   
+ 
+    }else{
+       let newlanded =  parseFloat(landed.value) + tierPrices[0]
+       let apiRate = parseFloat(newlanded * apidata['rates']['SGD']).toFixed(2)
+       console.log(apiRate)
+    }
+
+ 
+
+    // let usPrice = document.querySelectorAll('.USD1')
+    // let value = parseFloat(basePrice.value);
+    // for(let individualPrice of usPrice){
+    //     value -= parseFloat(tierVal);
+    //     tierPrices.push(parseFloat(value.toFixed(2)))
+    //     individualPrice.innerText = value.toFixed(2);
+    // }
+
 }
 
-const makeCalculation = function(price, tier){
+
+function calculateDiff(tierVal) {
+
+    tierPrices.push(parseFloat(basePrice.value))
+    let usPriceOriginal = document.querySelector('.USD')
+    usPriceOriginal.innerText = basePrice.value
+
+    let usPrice = document.querySelectorAll('.USD1')
+    let value = parseFloat(basePrice.value);
+    for(let individualPrice of usPrice){
+        value -= parseFloat(tierVal);
+        tierPrices.push(parseFloat(value.toFixed(2)))
+        individualPrice.innerText = value.toFixed(2);
+    }
+ 
+    console.log(tierPrices)
+
+    
+    fetch('https://api.exchangeratesapi.io/latest?base=USD')
+    .then(response => response.json())
+    .then(data =>  runExchange(data));
+
+}
+
+const makeCalculation = function (price, tier) {
     console.log(price.value)
     console.log(tier.value)
-   
+
     calculateDiff(tier.value)
-    
+
 }
 
-const triggerCalculationFunction = function(e){
+const triggerCalculationFunction = function (e) {
     e.preventDefault()
     makeCalculation(basePrice, tierDiff)
 }
@@ -32,6 +92,3 @@ const triggerCalculationFunction = function(e){
 
 calculateBtn.addEventListener('click', triggerCalculationFunction)
 
-
-
-// https://api.exchangeratesapi.io/latest?base=USD
